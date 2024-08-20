964d74b7ab92bffaf59e880699c0f709eb7cb4cc
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-08-20 10:30:56 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Qin Qin 2 Pro (s9863a1h10)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_s9863a1h10.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from s9863a1h10 device
+$(call inherit-product, device/duoqin/s9863a1h10/device.mk)
+
+PRODUCT_DEVICE := s9863a1h10
+PRODUCT_NAME := omni_s9863a1h10
+PRODUCT_BRAND := Qin
+PRODUCT_MODEL := Qin 2 Pro
+PRODUCT_MANUFACTURER := duoqin
+
+PRODUCT_GMS_CLIENTID_BASE := android-duoqin
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="s9863a1h10_Natv-user 9 PPR1.180610.011 251 release-keys"
+
+BUILD_FINGERPRINT := Qin/s9863a1h10_Natv/s9863a1h10:9/PPR1.180610.011/251:user/release-keys
